981f70663c9c0c76c6ae88d368edf071a2a56bb5
Dawid Derewońko <angeldeabyssi@gmail.com>
2023-11-30 13:57:30 +0100

    Creating new notes

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,9 +3,24 @@ import Image from "next/image";
 import {useUser} from "@clerk/clerk-react";
 import {Button} from "@/components/ui/button";
 import {PlusCircle} from "lucide-react";
+import {useMutation} from "convex/react";
+import {api} from "@/convex/_generated/api";
+import {toast} from "sonner";
 
 const DocumentsPage = () => {
     const {user} = useUser();
+    const create = useMutation(api.documents.create);
+
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating new note...",
+            success: "New note created!",
+            error: "Failed to create a new note."
+        })
+    }
 
     console.log(user)
 
@@ -26,7 +41,7 @@ const DocumentsPage = () => {
                 className="hidden dark:block"
             />
             <h2>Welcome to {user?.username}&apos;s Dejvotion</h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-4 w-4 mr-2" />
                 Create a note
             </Button>
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,15 +1,21 @@
 "use client"
 
-import {ChevronsLeft, MenuIcon} from "lucide-react";
+import {ChevronsLeft, MenuIcon, PlusCircle, Search, Settings} from "lucide-react";
 import React, {ElementRef, useEffect, useRef, useState} from "react";
-import { useMediaQuery } from "usehooks-ts";
+import {useMediaQuery} from "usehooks-ts";
 import {usePathname} from "next/navigation";
 import {cn} from "@/lib/utils";
 import {UserItem} from "@/app/(main)/_components/user-item";
+import {useMutation} from "convex/react";
+import {api} from "@/convex/_generated/api";
+import {Item} from "@/app/(main)/_components/item";
+import {toast} from "sonner";
+import {DocumentList} from "@/app/(main)/_components/document-list";
 
 export const Navigation = () => {
     const pathName = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -91,6 +97,16 @@ export const Navigation = () => {
         }
     }
 
+    const handleCreate = () => {
+        const promise = create({title: "Untitled"});
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note"
+        })
+    }
+
     return (
         <>
             <aside
@@ -112,10 +128,25 @@ export const Navigation = () => {
                     <ChevronsLeft className="h-6 w-6"/>
                 </div>
                 <div>
-                    <UserItem />
+                    <UserItem/>
+                    <Item label="Search"
+                          onClick={() => {
+                          }}
+                          isSearch
+                          icon={Search}
+                    />
+                    <Item label="Settings"
+                          onClick={() => {
+                          }}
+                          icon={Settings}
+                    />
+                    <Item label="New page"
+                          onClick={handleCreate}
+                          icon={PlusCircle}
+                    />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    <DocumentList />
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
@@ -134,7 +165,8 @@ export const Navigation = () => {
                 <nav
                     className="bg-transparent px-3 py-2 w-full"
                 >
-                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />
+                    {isCollapsed &&
+                        <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>
                     }
                 </nav>
             </div>
